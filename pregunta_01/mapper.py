8a51ebed86db07b922331a9b3450afa388d0eacf
NataliaPalacio <36682215+nata2490@users.noreply.github.com>
2022-06-05 22:26:53 -0500

    preg1
    commit preg1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    columns = line.split(',') # split line into parts
+    # the data is messy, only read those having correct column count
+    if len(columns) == 21: 
+        try:
+            column3 = columns[2]
+            #column3=sorted(column3)
+            print ("%s\t%s" % (column3, 1))
+        except ValueError:
+            pass
+       
