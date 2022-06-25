4a86c2824087c6b359b1581855d1b481ba45d34d
NataliaPalacio <36682215+nata2490@users.noreply.github.com>
2022-06-24 21:17:58 -0500

    COMMIT FINAL MAPPER

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -4,8 +4,9 @@
 import sys
 
 if __name__ == "__main__":
+
     for line in sys.stdin:
-        result = line.split()
-                
-        sys.stdout.write("{}\t1\n".format(result[1][5:7]))
+        data = line.split()[1]
+        month = data.split("-")[1]
+        sys.stdout.write("{}\t1\n".format(month))
 
