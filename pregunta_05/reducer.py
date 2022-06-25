370fee29209dc69ebfcd6ada5bd3c17f4176e964
NataliaPalacio <36682215+nata2490@users.noreply.github.com>
2022-06-24 21:03:00 -0500

    COMMIT MAP

--- pregunta_05/reducer.py
+++ pregunta_05/reducer.py
@@ -16,8 +16,8 @@ if __name__ == '__main__':
              total += val
         else:
             if curkey is not None:
-                sys.stdout.write("{}\t{}\n".format(curkey, total))
+                sys.stdout.write("{}\t{}\n".format(curkey,total))
             curkey = key
             total = val
-    sys.stdout.write("{}\t{}\n".format(curkey, total))
+    sys.stdout.write("{}\t{}\n".format(curkey,total))
 
